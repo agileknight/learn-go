a5c698fb83c98f6844a0a9894851d585fb647eff
Philipp Meier <philipp.meier@codecentric.de>
2015-01-05 09:09:46 +0100

    format

--- camelup/game_test.go
+++ camelup/game_test.go
@@ -9,15 +9,15 @@ func TestInit(t *testing.T) {
 		{2},
 		{4},
 	}
-	
-	for _,c := range cases {
+
+	for _, c := range cases {
 		game := Init(c.numPlayers)
 		if spliceLen := len(game.players); spliceLen != c.numPlayers {
 			t.Errorf("Player splice was size %d for num players %d", spliceLen, c.numPlayers)
 		}
-		
-		for i,p := range game.players {
-			if (p.money != PlayerStartMoney) {
+
+		for i, p := range game.players {
+			if p.money != PlayerStartMoney {
 				t.Errorf("Player %d got initialized with money %d not matching start amount %d", i, p.money, PlayerStartMoney)
 			}
 		}
